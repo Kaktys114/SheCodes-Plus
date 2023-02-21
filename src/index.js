3a25d0c04f00945242499114008720ccefddf55c
Дар'я <101970312+Kaktys114@users.noreply.github.com>
2023-02-21 22:29:59 +0100

    make js-code cleaner

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ let months = [
   "December",
 ];
 
+let currentData = new Date();
+
+// current date⬇️
+
 function getFormattedDate(date) {
   let weekDay = days[date.getDay()];
   let todayDate = date.getDate();
@@ -33,90 +37,69 @@ function getFormattedDate(date) {
   return `${weekDay}, ${todayDate} ${month}`;
 }
 
+document.querySelector("#todayDate").innerHTML = getFormattedDate(currentData);
+
+// current date⬆️
+
+// current time⬇️
+
 function getFormattedTime(time) {
   let hours = time.getHours();
+
   if (hours < 10) {
     hours = `0${hours}`;
   }
 
   let minutes = time.getMinutes();
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
   return `${hours}:${minutes}`;
 }
+document.querySelector("#timeNow").innerHTML = getFormattedTime(currentData);
 
-let currentData = new Date();
+// current time⬆️
 
-// current date
-let date = document.querySelector("#todayDate");
-date.innerHTML = getFormattedDate(currentData);
-
-// current time
-let time = document.querySelector("#timeNow");
-time.innerHTML = getFormattedTime(currentData);
-
-// Searching for a new city
-function updateTemperature(response) {
-  let temperature = document.querySelector(`#temperatureNow`);
-  let searchTemperature = Math.round(response.data.main.temp);
-  temperature.innerHTML = `${searchTemperature}℃`;
+// Searching for a new city⬇️
+function updateAfterSearching(response) {
+  document.querySelector(`#temperatureNow`).innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = response.data.name;
 }
 
-function updateAfterSearch(event) {
+function updateWeather(userCity) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateAfterSearching);
+}
+
+function searchCity(event) {
   event.preventDefault();
-  let userCityName = document.querySelector("#userCity-input");
-  let city = document.querySelector("#city");
-  let searchCity = userCityName.value;
-  city.innerHTML = searchCity;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(updateTemperature);
+  let userCity = document.querySelector("#userCity-input").value;
+  updateWeather(userCity);
 }
 
-let cityForm = document.querySelector("#search-form");
-cityForm.addEventListener("submit", updateAfterSearch);
+document.querySelector("#search-form").addEventListener("submit", searchCity);
+// Searching for a new city ⬆️
 
-// // Convert temperature
-// let temperature = document.querySelector("#temperatureNow");
-
-// function convertToFahrenheit(event) {
-//   event.preventDefault();
-//   temperature.innerHTML = `78℉`;
-// }
-
-// let fahrenheit = document.querySelector("#fahrenheit-link");
-// fahrenheit.addEventListener("click", convertToFahrenheit);
-
-// function convertToCelcius(event) {
-//   event.preventDefault();
-//   temperature.innerHTML = "22℃";
-// }
-
-// let celcius = document.querySelector("#celcius-link");
-// celcius.addEventListener("click", convertToCelcius);
-
-// current location
-function showCurrentTemperature(response) {
-  let city = document.querySelector("#city");
-  let currentCity = response.data.name;
-  city.innerHTML = currentCity;
-  let temperature = document.querySelector(`#temperatureNow`);
-  let currentTemperature = Math.round(response.data.main.temp);
-  temperature.innerHTML = `${currentTemperature}℃`;
-}
-
+// click on the "current"-button⬇️
 function getCurrentLocation(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
   let unit = `metric`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(showCurrentTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(updateAfterSearching);
 }
 
-function getGeoposition() {
+function getGeoposition(event) {
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(getCurrentLocation);
 }
 
-let currentButton = document.querySelector(`.current-btn`);
-currentButton.addEventListener("click", getGeoposition);
+document
+  .querySelector(`.current-btn`)
+  .addEventListener("click", getGeoposition);
+
+// click on the "current"-button⬇⬆️
+
+updateWeather("Berlin");
